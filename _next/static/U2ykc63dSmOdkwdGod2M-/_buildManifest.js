761bd47d7c6aaaca740fe25098365eb0eb96b93a
Charlotte Jacques <charlottej3333@gmail.com>
2025-11-02 14:25:11 -0800

    Updates

--- _next/static/U2ykc63dSmOdkwdGod2M-/_buildManifest.js
+++ _next/static/U2ykc63dSmOdkwdGod2M-/_buildManifest.js
@@ -1,10 +1,15 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/a9e10e398085466a.js"
+    "./static/chunks/95359849bb611fe1.js"
   ],
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/charlottejacques3.github.io//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
